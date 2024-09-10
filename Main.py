7a425e56e2ec7bfc99eb96f433537383cc1467ad
UIT.23730214 <23730214@ms.uit.edu.vn>
2024-09-10 20:39:05 +0700

    Update Main.py
    a update lại các tên hàm và tên biến thành tiếng việt hết để mọi người dể làm, trước khi code thì nhớ checkout source mới nhất về neh1

--- Main.py
+++ Main.py
@@ -3,22 +3,22 @@ import pygame
 pygame.init()
 
 # // Lam mau sac cho game
-white = (255, 255, 255)
-black = (0, 0, 0)
-red = (255, 0, 0)
+trang = (255, 255, 255)
+den = (0, 0, 0)
+do = (255, 0, 0)
 
 # # Thiet lap cua so cua game
-dis_width = 800
-dis_height = 600
-dis = pygame.display.set_mode((dis_width, dis_height))
+chieu_rong_cua_so = 800
+chieu_cao_cua_so = 600
+cua_so = pygame.display.set_mode((chieu_rong_cua_so, chieu_cao_cua_so))
 pygame.display.set_caption('Game Con Rắn - Nhóm 2')
 
 # set trang thái bằng false 
-game_over = False
+tro_choi_ket_thuc = False
 
 # vị trí ban đầu con rắn xuất hiện
-x1 = dis_width // 2
-y1 = dis_height // 2
+x1 = chieu_rong_cua_so // 2
+y1 = chieu_cao_cua_so // 2
 
 # kích thước o vuong == con ran
 kich_thuoc_o_vuong = 10
@@ -29,11 +29,11 @@ y1_thay_doi = 0
 
 
 #tốc độ khung hình
-clock = pygame.time.Clock()
-snake_speed = 15
+dong_ho = pygame.time.Clock()
+toc_do_ran = 15
 
 # vòng lặp chính của trò chơi
-while not game_over:
+while not tro_choi_ket_thuc:
     for su_kien in pygame.event.get():
         if su_kien.type == pygame.QUIT:
             tro_choi_ket_thuc = True 
@@ -56,17 +56,17 @@ while not game_over:
     y1 += y1_thay_doi
 
     # nếu rắn chạm vào tường thì kết thúc trò chơi
-    if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+    if x1 >= chieu_rong_cua_so or x1 < 0 or y1 >= chieu_cao_cua_so or y1 < 0:
         game_over = True
 
     # màn hình và rắn
-    dis.fill(white)
-    pygame.draw.rect(dis, black, [x1, y1, kich_thuoc_o_vuong, kich_thuoc_o_vuong])
+    cua_so.fill(trang)
+    pygame.draw.rect(cua_so, den, [x1, y1, kich_thuoc_o_vuong, kich_thuoc_o_vuong])
 
     pygame.display.update()
 
     # tốc độ trò chơi
-    clock.tick(snake_speed)
+    dong_ho.tick(toc_do_ran)
 
 pygame.quit()
 quit()
